d8b2fd9a12f092f949a85bfb4918307868eb4cd6
Scott Newton <scott.newton@ft.com>
2016-02-17 15:06:14 +0000

    Fixed breaking change from neo-go-utils and http-handlers package re-work

--- app.go
+++ app.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/Financial-Times/base-ft-rw-app-go/baseftrwapp"
 	"github.com/Financial-Times/go-fthealth/v1a"
-	"github.com/Financial-Times/http-handlers-go"
+	"github.com/Financial-Times/http-handlers-go/httphandlers"
 	"github.com/Financial-Times/public-organisations-api/organisations"
 
 	"fmt"
