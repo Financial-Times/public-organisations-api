6272446f32f315a48746044654fc6da4c2676704
Scott Newton <scott.newton@ft.com>
2018-05-23 13:44:12 +0100

    Increase performance

--- organisations/cypher.go
+++ organisations/cypher.go
@@ -145,7 +145,8 @@ func (pcw CypherDriver) ReadNewFormat(uuid string) (organisation Organisation, f
 	}{}
 	query := &neoism.CypherQuery{
 		Statement: `
-			MATCH (Thing{uuid:{uuid}})-[:EQUIVALENT_TO]->(canonical:Organisation)
+			MATCH (t:Thing{uuid:{uuid}})
+			MATCH (t)-[:EQUIVALENT_TO]->(canonical:Organisation)
 			OPTIONAL MATCH (canonical)<-[:EQUIVALENT_TO]-(source:Organisation)
 			OPTIONAL MATCH (source)-[:HAS_CLASSIFICATION]->(industryClassification:IndustryClassification)
 			OPTIONAL MATCH (source)-[:SUB_ORGANISATION_OF]->(parentOrganisation:Organisation)
