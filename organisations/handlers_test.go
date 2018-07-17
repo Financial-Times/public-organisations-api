8f3d61710168c7c187e1091c9b57eeb56df481e7
sisidovski <arerreee@gmail.com>
2018-07-17 17:58:07 +0100

    Resgtore existing test

--- organisations/handlers_test.go
+++ organisations/handlers_test.go
@@ -3,6 +3,7 @@ package organisations
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
@@ -24,12 +25,20 @@ const (
 	alternateUUID              string = "00000000-0000-002a-0000-00000000002b"
 )
 
-type mockOrganisationDriver struct{}
-
 type mockHTTPClient struct {
 	resp       string
 	statusCode int
 	err        error
+}
+
+type testCase struct {
+	name         string
+	url          string
+	clientCode   int
+	clientBody   string
+	clientError  error
+	expectedCode int
+	expectedBody string
 }
 
 func (mhc *mockHTTPClient) Do(req *http.Request) (resp *http.Response, err error) {
@@ -37,27 +46,9 @@ func (mhc *mockHTTPClient) Do(req *http.Request) (resp *http.Response, err error
 	return &http.Response{Body: cb, StatusCode: mhc.statusCode}, mhc.err
 }
 
-func (driver mockOrganisationDriver) Read(id string) (organisation Organisation, found bool, err error) {
-	return Organisation{Thing: Thing{ID: canonicalUUID, APIURL: ""}}, isFound, nil
-}
-
-func (driver mockOrganisationDriver) CheckConnectivity() error {
-	return nil
-}
-
 func TestHandlers(t *testing.T) {
 	var mockClient mockHTTPClient
 	router := mux.NewRouter()
-
-	type testCase struct {
-		name         string
-		url          string
-		clientCode   int
-		clientBody   string
-		clientError  error
-		expectedCode int
-		expectedBody string
-	}
 
 	invalidUUID := testCase{
 		"Get organisation - Invalid UUID results in error",
@@ -155,60 +146,33 @@ func TestHandlers(t *testing.T) {
 	}
 }
 
+func TestHeadersOKOnFoundForCanonicalNode(t *testing.T) {
+	var mockClient mockHTTPClient
+	mockClient.resp = getBasicOrganisationAsConcept
+	mockClient.statusCode = 200
+	mockClient.err = nil
+
+	CacheControlHeader = expectedCacheControlHeader
+
+	router := mux.NewRouter()
+	bh := NewHandler(&mockClient, "localhost:8080/concepts")
+	bh.RegisterHandlers(router)
+
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/organisations/d6b12f0c-bf3f-4045-a07b-1e4e49103fd6", nil)
+
+	router.ServeHTTP(rec, req)
+	fmt.Print(rec.Body)
+
+	assert.Equal(t, expectedCacheControlHeader, rec.Header().Get("Cache-Control"))
+	assert.Equal(t, "application/json; charset=UTF-8", rec.Header().Get("Content-Type"))
+}
+
 func transformBody(testBody string) string {
 	stripNewLines := strings.Replace(testBody, "\n", "", -1)
 	stripTabs := strings.Replace(stripNewLines, "\t", "", -1)
 	return stripTabs + "\n"
 }
-
-// func init() {
-// 	OrganisationDriver = mockOrganisationDriver{}
-// 	CacheControlHeader = expectedCacheControlHeader
-// 	r := mux.NewRouter()
-// 	r.HandleFunc("/organisations/{uuid}", GetOrganisation).Methods("GET")
-// 	server = httptest.NewServer(r)
-// 	organisationsURL = fmt.Sprintf("%s/organisations", server.URL) //Grab the address for the API endpoint
-// 	isFound = true
-// }
-//
-// func TestHeadersOKOnFoundForCanonicalNode(t *testing.T) {
-// 	assert := assert.New(t)
-// 	isFound = true
-// 	req, _ := http.NewRequest("GET", organisationsURL+"/"+canonicalUUID, nil)
-// 	res, err := http.DefaultClient.Do(req)
-// 	assert.NoError(err)
-// 	assert.EqualValues(200, res.StatusCode)
-// 	assert.Equal(expectedCacheControlHeader, res.Header.Get("Cache-Control"))
-// 	assert.Equal("application/json; charset=UTF-8", res.Header.Get("Content-Type"))
-// }
-//
-// func noRedirect(req *http.Request, via []*http.Request) error {
-// 	return errors.New("Don't redirect!")
-// }
-//
-// func TestRedirectHappensOnFoundForAlternateNode(t *testing.T) {
-// 	assert := assert.New(t)
-// 	isFound = true
-// 	req, _ := http.NewRequest("GET", organisationsURL+"/"+alternateUUID, nil)
-// 	client := &http.Client{
-// 		CheckRedirect: noRedirect,
-// 	}
-// 	res, err := client.Do(req)
-// 	assert.Contains(err.Error(), "Don't redirect!")
-// 	assert.EqualValues(301, res.StatusCode)
-// 	assert.Equal("/organisations/"+canonicalUUID, res.Header.Get("Location"))
-// 	assert.Equal("application/json; charset=UTF-8", res.Header.Get("Content-Type"))
-// }
-//
-// func TestReturnNotFoundIfOrgNotFound(t *testing.T) {
-// 	assert := assert.New(t)
-// 	isFound = false
-// 	req, _ := http.NewRequest("GET", organisationsURL+"/"+canonicalUUID, nil)
-// 	res, err := http.DefaultClient.Do(req)
-// 	assert.NoError(err)
-// 	assert.EqualValues(404, res.StatusCode)
-// 	assert.Equal("application/json; charset=UTF-8", res.Header.Get("Content-Type"))
-// }
 
 var getBasicOrganisationAsConcept = `{
 	"id": "http://api.ft.com/things/d6b12f0c-bf3f-4045-a07b-1e4e49103fd6",
